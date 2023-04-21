21636236be72eb79376537150fb81bd3e85d3383
Anton <antonkzavcenco05@gmail.com>
2023-04-21 20:21:15 +0200

    add loader with lottie

--- app/ui/Loader/FullScreenLoader.tsx
+++ app/ui/Loader/FullScreenLoader.tsx
@@ -1,9 +1,15 @@
-import { ActivityIndicator, View } from 'react-native'
+import Lottie from 'lottie-react-native'
+import { View } from 'react-native'
+import loader from '../../assets/loader.json'
 
 const FullScreenLoader = () => {
 	return (
 		<View className={'h-full w-full'}>
-			<ActivityIndicator size='large' className={'h-full w-full'} color='#fff' />
+			<Lottie
+				loop
+				source={loader}
+				autoPlay={true}
+			/>
 		</View>
 	)
 }

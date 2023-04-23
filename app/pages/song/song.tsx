9ae8190de847243d7caffcb4353ac4768a0ccd9a
Anton <antonkzavcenco05@gmail.com>
2023-04-23 20:34:02 +0200

    add small fix for merge branch

--- app/pages/song/song.tsx
+++ app/pages/song/song.tsx
@@ -1,6 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
-import TrackPlayer, { State, usePlaybackState } from 'react-native-track-player'
+import TrackPlayer, {
+	AppKilledPlaybackBehavior,
+	Capability,
+	RepeatMode,
+	State,
+	usePlaybackState
+} from 'react-native-track-player'
+import { skipToPrevious } from 'react-native-track-player/lib/trackPlayer'
 import { useTypedRoute } from '../../hook/useTypedRoute'
 import { useDownloadTrackMp3Query, useGetTrackByIdQuery, useGetTrackMp3ByNameQuery } from '../../redux/api/song/song'
 import NavigateHeader from '../../ui/header/navigateHeader'
@@ -15,64 +22,99 @@ const Song = () => {
 	const { data: songData } = useGetTrackByIdQuery(params.id)
 	const { data: songLyrics } = useGetTrackMp3ByNameQuery(songData?.title as string)
 	const { data: mp3 } = useDownloadTrackMp3Query(songLyrics?.result.find((item) => item.user.username === songData?.artist.name || item.title === songData?.title)?.url as string)
-	const playerState = usePlaybackState()
-	useEffect(() => {
-		const play = async () => {
-			await TrackPlayer.getActiveTrack().then((track) => {
-				console.log(track)
-			})
-			if (!songData || !mp3) return <FullScreenLoader />
-			await TrackPlayer.reset()
-			await TrackPlayer.getActiveTrack().then((track) => {
-				if (track === null) {
-					TrackPlayer.add({
-						id: songData.id.toString(),
-						url: mp3.music.download_url,
-						title: songData.title,
-						artist: songData.artist.name,
-						artwork: songData.album.cover_big
-					})
-				}
-			})
-			await TrackPlayer.play()
-			
-			
-			await TrackPlayer.add({
-				id: songData.id,
-				url: mp3.music.download_url,
-				title: songData.title,
-				artist: songData.artist.name,
-				artwork: songData.album.cover_big
-			})
-			await TrackPlayer.getPlaybackState().then((state) => {
-				console.log(state)
-			})
-			await TrackPlayer.getVolume().then((volume) => {
-				console.log(volume)
-			})
-			await TrackPlayer.play()
-		}
-		play()
-	}, [])
-	console.log(playerState, State.Playing)
+	const playBackState = usePlaybackState()
+	const [isPlayerReady, setIsPlayerReady] = useState(false)
 	
-	async function handlePlayPress() {
-		if (playerState.state === State.Playing) {
-			await TrackPlayer.pause()
-		} else {
-			await TrackPlayer.play()
+	async function setupPlayer() {
+		let isSetup = false
+		try {
+			await TrackPlayer.getActiveTrack()
+			isSetup = true
+		} catch {
+			await TrackPlayer.setupPlayer()
+			await TrackPlayer.updateOptions({
+				android: {
+					appKilledPlaybackBehavior:
+					AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification
+				},
+				capabilities: [
+					Capability.Play,
+					Capability.Pause,
+					Capability.SkipToNext,
+					Capability.SkipToPrevious,
+					Capability.SeekTo
+				],
+				compactCapabilities: [
+					Capability.Play,
+					Capability.Pause,
+					Capability.SkipToNext
+				],
+				progressUpdateEventInterval: 2
+			})
+			
+			isSetup = true
+		} finally {
+			return isSetup
 		}
 	}
 	
-	if (!songData || !mp3) return <FullScreenLoader />
+	async function addTracks() {
+		if (!songData || !mp3) return
+		if (playBackState.state === State.Playing) {
+			await TrackPlayer.clearNowPlayingMetadata()
+		}
+		await TrackPlayer.add(
+			{
+				id: songData.id,
+				url: mp3.music.download_url,
+				title: songData?.title,
+				artist: songData.artist.name,
+				artwork: songData.album.cover_big
+			}
+		).then(() => {
+			console.log({
+				id: songData.id,
+				url: mp3.music.download_url,
+				title: songData?.title,
+				artist: songData.artist.name,
+				artwork: songData.album.cover_big
+				
+			})
+		})
+		await TrackPlayer.setRepeatMode(RepeatMode.Queue)
+	}
+	
+	useEffect(() => {
+		async function setup() {
+			let isSetup = await setupPlayer()
+			
+			const queue = await TrackPlayer.getQueue()
+			if (isSetup && queue.length >= 0) {
+				await addTracks()
+			}
+			
+			setIsPlayerReady(isSetup)
+		}
+		
+		setup()
+	}, [])
+	
+	
+	if (!songData || !mp3 || !isPlayerReady) return <FullScreenLoader />
 	return <Layout>
 		<NavigateHeader logoSize={30} className='mb-4' />
 		<Title text={songData.title} size={30} className={'mx-auto mb-4'} />
 		<UImage source={songData.album.cover_big} className={'rounded-full mx-auto'} width={300} height={300} />
 		<View className='justify-between flex-row'>
-			<Icon onPress={() => TrackPlayer.skipToPrevious()} name='caret-back' />
-			<Icon onPress={handlePlayPress}
-			      name={(playerState.state === State.Playing) ? 'pause' : 'play'} />
+			<Icon onPress={() => skipToPrevious()} name='caret-back' />
+			<Icon onPress={() => {
+				if (playBackState.state === State.Playing) {
+					TrackPlayer.pause()
+				} else {
+					TrackPlayer.play()
+				}
+			}}
+			      name={(playBackState.state === State.Playing) ? 'pause' : 'play'} />
 			<Icon onPress={() => TrackPlayer.skipToNext()} name='caret-forward' />
 		</View>
 	</Layout>
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import App from './App'
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
-TrackPlayer.registerPlaybackService(() => require('./service.js'));
+registerRootComponent(App)
+TrackPlayer.registerPlaybackService(() => require('./service'))
